4ffa4f232aea801641b1ad888567f282450530e6
CiscG <80540828+CiscG@users.noreply.github.com>
2024-11-08 21:49:28 -0300

    Update Pessoa.java
    Signed-off-by: CiscG <80540828+CiscG@users.noreply.github.com>

--- com.banco.dev.app/src/Pessoa.java
+++ com.banco.dev.app/src/Pessoa.java
@@ -2,7 +2,10 @@ package com.banco.dev.app
 public class Pessoa {
   String nome;
   int idade;
-
+  String tipoDePessoa;
+  int id;
+  float renda;
+  
   Pessoa(){
   }
   Pessoa(String nome, int idade){
