41222b35c8223cf81d296ce78b9ad18de108663d
KarolSchroder <k4r0lschr0der@gmail.com>
2024-11-21 23:08:20 -0300

    Update ContaPoupanca.java
    Signed-off-by: KarolSchroder <k4r0lschr0der@gmail.com>

--- com.banco.dev.app/src/com/banco/dev/app/ContaPoupanca.java
+++ com.banco.dev.app/src/com/banco/dev/app/ContaPoupanca.java
@@ -1,4 +1,13 @@
 package com.banco.dev.app;
 public class ContaPoupanca extends Conta{
-
+  float taxaPoupanca;
+}
+ContaPoupanca(){
+}
+ContaPoupanca(Pessoa cliente, int agencia, float saldo, int numeroConta, float taxaPoupanca){
+  this.cliente = cliente;
+  this.saldo = saldo;
+  this.agencia = agencia;
+  this.numeroConta = numeroConta;
+  this.taxaPoupanca = taxaPoupanca
 }
