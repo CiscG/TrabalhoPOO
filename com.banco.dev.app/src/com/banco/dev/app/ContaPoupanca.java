26fe81b2f2c10068437db12410f43e8daf3e81ec
CiscG <fgss98@gmail.com>
2024-11-22 05:06:07 -0300

    Att

--- com.banco.dev.app/src/com/banco/dev/app/ContaPoupanca.java
+++ com.banco.dev.app/src/com/banco/dev/app/ContaPoupanca.java
@@ -12,4 +12,8 @@ public class ContaPoupanca extends Conta {
     this.numeroConta = numeroConta;
     this.taxaPoupanca = taxaPoupanca;
   }
+  void AtualizarValorConta(){
+    ///Será o metodo para a atualização do valor da conta baseado na taxa da conta poupança
+    ///Darei uma olhada nisso mais tarde
+  }
 }
