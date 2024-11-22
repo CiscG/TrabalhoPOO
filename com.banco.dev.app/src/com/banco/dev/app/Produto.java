49e53ae5b04bba16b86c2e75e11d2a464aa4cd1b
KarolSchroder <k4r0lschr0der@gmail.com>
2024-11-21 22:59:08 -0300

    Update Produto.java
    Signed-off-by: KarolSchroder <k4r0lschr0der@gmail.com>

--- com.banco.dev.app/src/com/banco/dev/app/Produto.java
+++ com.banco.dev.app/src/com/banco/dev/app/Produto.java
@@ -4,3 +4,11 @@ public class Produto {
   float taxa;
   float valor;
 }
+Produto(){
+}
+Produto(String nome, float taxa, float valor){
+  this.nome = nome;
+  this.taxa = taxa;
+  this.valor = valor;
+}
+///Me perdi na uml :) 
