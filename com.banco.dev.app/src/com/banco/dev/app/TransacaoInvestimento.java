8154f012c3b5d18afe0c434376bfc4bea3a3a3df
KarolSchroder <k4r0lschr0der@gmail.com>
2024-11-21 22:52:51 -0300

    Update TransacaoInvestimento.java
    Signed-off-by: KarolSchroder <k4r0lschr0der@gmail.com>

--- com.banco.dev.app/src/com/banco/dev/app/TransacaoInvestimento.java
+++ com.banco.dev.app/src/com/banco/dev/app/TransacaoInvestimento.java
@@ -3,6 +3,7 @@ public class TransacaoInvestimento extends Transacao {
     String status;
     TransacaoInvestimento(){}
     TransacaoInvestimento(float valor, String destino){
-
+        this.valor =  valor;
+        this.destino = destino;
     }
 }
