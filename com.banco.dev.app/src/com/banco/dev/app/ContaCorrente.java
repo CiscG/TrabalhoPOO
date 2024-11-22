ab4d8a8f2b23af6bfbabf620332103ec95959e3a
KarolSchroder <k4r0lschr0der@gmail.com>
2024-11-21 23:21:02 -0300

    Update ContaCorrente.java
    Signed-off-by: KarolSchroder <k4r0lschr0der@gmail.com>

--- com.banco.dev.app/src/com/banco/dev/app/ContaCorrente.java
+++ com.banco.dev.app/src/com/banco/dev/app/ContaCorrente.java
@@ -1,3 +1,17 @@
 package com.banco.dev.app;
 public class ContaCorrente extends Conta {
+  int limite;
+}
+
+///Arraylist faltando de novo :)
+///Inclusive, acho que organizei o código errado, depois a gente vê
+
+ContaCorrente(){
+}
+ContaCorrente(Pessoa cliente, int agencia, float saldo, int numeroConta, int limite){
+  this.cliente = cliente;
+  this.saldo = saldo;
+  this.agencia = agencia;
+  this.numeroConta = numeroConta;
+  this.limite = limite;
 }
