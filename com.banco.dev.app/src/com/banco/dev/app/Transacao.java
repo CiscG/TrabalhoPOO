cb34497879eea2232bce88d1f2c96ef365502172
KarolSchroder <k4r0lschr0der@gmail.com>
2024-11-28 17:03:39 -0300

    Update Transacao.java
    Signed-off-by: KarolSchroder <k4r0lschr0der@gmail.com>

--- com.banco.dev.app/src/com/banco/dev/app/Transacao.java
+++ com.banco.dev.app/src/com/banco/dev/app/Transacao.java
@@ -16,13 +16,13 @@ public class Transacao {
         this.data = data;
         this.destino = destino;
     }
-    void SetValor(int valor){
+    public void SetValor(int valor){
         this.valor = valor;
     }
-    void SetData (Date data){
+    public void SetData (Date data){
         this.data = data;
     }
-    void SetDestino(String destino){
+    public void SetDestino(String destino){
         this.destino = destino;
     }
 }
