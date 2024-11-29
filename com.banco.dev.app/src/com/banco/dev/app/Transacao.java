85499ee99f2fd5836dcd33e92b25390fb3b17924
CiscG <80540828+CiscG@users.noreply.github.com>
2024-11-29 19:45:19 -0300

    Update Transacao.java
    Signed-off-by: CiscG <80540828+CiscG@users.noreply.github.com>

--- com.banco.dev.app/src/com/banco/dev/app/Transacao.java
+++ com.banco.dev.app/src/com/banco/dev/app/Transacao.java
@@ -3,9 +3,9 @@ package com.banco.dev.app;
 import java.util.*;
 
 public class Transacao {
-    float valor;
-    Date data;
-    String destino;
+    public float valor;
+    public Date data;
+    public String destino;
 
     //Construtores da classe mãe
     Transacao(){}
