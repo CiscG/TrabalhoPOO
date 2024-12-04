40ff30bb651437ce00fc603641cc30f2e508a9bd
KarolSchroder <k4r0lschr0der@gmail.com>
2024-12-04 18:39:06 -0300

    Update TransacaoCorrente.java
    Signed-off-by: KarolSchroder <k4r0lschr0der@gmail.com>

--- com.banco.dev.app/src/com/banco/dev/app/TransacaoCorrente.java
+++ com.banco.dev.app/src/com/banco/dev/app/TransacaoCorrente.java
@@ -5,13 +5,13 @@ public class TransacaoCorrente extends Transacao{
     public String destinatario;
     //Construtor da classe filha, pois a classe filha não herda construtor
     TransacaoCorrente(float valor, int destino, int destinatario, Date data){}
-    TransacaoCorrente(float valor, String destinatario, String destino, Date data){
+    TransacaoCorrente(float valor, String destinatario, int destino, Date data){
         this.valor = valor;
         this.destinatario = destinatario;
         this.destino = destino;
         this.data = data;
     }
-    public void SetDestinatario(String destinatario){
+    public void setDestinatario(String destinatario){
         this.destinatario = destinatario;
     }
        public String getDestinatario(){
